0085ba983b995ca0a2e793a6e845ccbdfb968685
Damian <info@timersys.com>
2019-06-26 16:04:26 -0300

    wizard addons fixes

--- admin/partials/setup-wizard-addons.php
+++ admin/partials/setup-wizard-addons.php
@@ -4,15 +4,16 @@
 
 		<?php do_action( 'geot/wizard/addons/before' ); ?>
 
-		<p><?php _e( 'The wizard below will help you configure your Geotargeting plugin and start working quickly.', 'letsgo' ); ?></p>
+		<p><?php _e( 'Choose which addons you want to enable. You can change all settings later anyway.', 'letsgo' ); ?></p>
+
 
 		<div class="location-row">
-			<label for="geo-flags" class="location-label">
-				<input type="checkbox" id="geo-flags" name="geot_addons[geo-flags]" value="1"
-				       class="location-checkbox" <?php echo checked( $opts['geo-flags'], true ); ?> />
-				<?php _e( 'Geo Flags', 'geot' ); ?>
+			<label for="geo-redirects" class="location-label">
+				<input type="checkbox" id="geo-redirects" name="geot_addons[geo-redirects]" value="1"
+				       class="location-checkbox" <?php echo checked( $opts['geo-redirects'], true ); ?> />
+				<?php _e( 'Geo Redirects', 'geot' ); ?>
 			</label>
-			<div class="location-help"><?php _e( 'Country Flags based on user location', 'geot' ); ?></div>
+			<div class="location-help"><?php printf( __( '<a href="%s" target="_blank">Redirect users</a> based on location and many other options.', 'geot' ), 'https://geotargetingwp.com/geo-redirects' ); ?></div>
 		</div>
 
 		<div class="location-row">
@@ -21,16 +22,7 @@
 				       class="location-checkbox" <?php echo checked( $opts['geo-links'], true ); ?> />
 				<?php _e( 'Geo Links', 'geot' ); ?>
 			</label>
-			<div class="location-help"><?php _e( 'Geo Links for WordPress will let you geo-target your affiliate links', 'geot' ); ?></div>
-		</div>
-
-		<div class="location-row">
-			<label for="geo-redirects" class="location-label">
-				<input type="checkbox" id="geo-redirects" name="geot_addons[geo-redirects]" value="1"
-				       class="location-checkbox" <?php echo checked( $opts['geo-redirects'], true ); ?> />
-				<?php _e( 'Geo Redirects', 'geot' ); ?>
-			</label>
-			<div class="location-help"><?php _e( 'Create redirects based on Countries, Cities or States. Add multiple rules', 'geot' ); ?></div>
+			<div class="location-help"><?php printf( __( '<a href="%s" target="_blank">Geo Links</a> for WordPress will let you geo-target your affiliate links.', 'geot' ), 'https://geotargetingwp.com/geo-links' ); ?></div>
 		</div>
 
 		<div class="location-row">
@@ -39,9 +31,16 @@
 				       class="location-checkbox" <?php echo checked( $opts['geo-blocker'], true ); ?> />
 				<?php _e( 'Geo Blocker', 'geot' ); ?>
 			</label>
-			<div class="location-help"><?php _e( 'Geo Blocker let you block access to your site based on geolocation', 'geot' ); ?></div>
+			<div class="location-help"><?php printf( __( '<a href="%s" target="_blank">Geo Blocker</a> let you block access to your site based on geolocation.', 'geot' ), 'https://geotargetingwp.com/geo-blocker' ); ?></div>
 		</div>
-
+		<div class="location-row">
+			<label for="geo-flags" class="location-label">
+				<input type="checkbox" id="geo-flags" name="geot_addons[geo-flags]" value="1"
+				       class="location-checkbox" <?php echo checked( $opts['geo-flags'], true ); ?> />
+				<?php _e( 'Geo Flags', 'geot' ); ?>
+			</label>
+			<div class="location-help"><?php  printf( __( 'Display <a href="%s" target="_blank">country flags</a> based on user location.', 'geot' ), 'https://geotargetingwp.com/geo-flags' ); ?></div>
+		</div>
 		<?php do_action( 'geot/wizard/addons/after' ); ?>
 
 		<div class="location-row text-center">
