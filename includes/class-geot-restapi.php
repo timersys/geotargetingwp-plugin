8233905ab5c5025d281a7d583161c67e65d2fa83
gonzalesc <alex@gonzalesc.org>
2020-02-13 14:21:30 -0300

    create RestApi class

---
+++ includes/class-geot-restapi.php
@@ -0,0 +1,25 @@
+<?php
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Rest Api
+ *
+ * @link       https://geotargetingwp.com/geotargeting-pro
+ * @since      1.6.3
+ *
+ * @package    GeoTarget
+ * @subpackage GeoTarget/includes
+ * @author     Damian Logghe
+ */
+class GeotWP_RestApi {
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct() {
+
+	}
+}		
