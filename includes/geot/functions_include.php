bc74df12e65f27a79072b06e4cecf413ba734b66
Damian Logghe <info@timersys.com>
2020-02-19 14:31:44 -0300

    removed unused file

--- includes/geot/functions_include.php
+++ includes/geot/functions_include.php
@@ -8,7 +8,6 @@ if ( ! function_exists( 'GeotCore\toArray' ) ) {
 	require __DIR__ . '/filters.php';
 	require __DIR__ . '/global-functions.php';
 	require __DIR__ . '/database.php';
-	require __DIR__ . '/plugins.php';
 }
 
 // Upgrades
