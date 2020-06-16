18ccb534fd5e2186269114ca0c4372e78adb5241
Damian Logghe <info@timersys.com>
2020-06-16 18:02:25 -0300

    ipv6 in ranges

--- includes/class-geot-helper.php
+++ includes/class-geot-helper.php
@@ -35,7 +35,7 @@ class GeotWP_Helper {
 					p.post_status = 'publish' AND pm.meta_key = '_geot_post' AND
 					pm2.meta_key = 'geot_options' AND pm.meta_value != ''
 				";
-		if( $pid ) {
+		if ( $pid ) {
 			$sql .= " AND p.ID = " . filter_var( $pid, FILTER_VALIDATE_INT );
 		}
 
@@ -91,8 +91,8 @@ class GeotWP_Helper {
 		}
 
 		if ( ! empty( $opts['zipcodes'] ) || ! empty( $opts['zip_region'] ) ) {
-			$zipcodes 	= ! empty( $opts['zipcodes'] ) ? $opts['zipcodes'] : '';
-			$regions 	= ! empty( $opts['zip_region'] ) ? $opts['zip_region'] : '';
+			$zipcodes       = ! empty( $opts['zipcodes'] ) ? $opts['zipcodes'] : '';
+			$regions        = ! empty( $opts['zip_region'] ) ? $opts['zip_region'] : '';
 			$zipcode_target = geot_target_zip( $zipcodes, $regions );
 
 			if ( $mode == 'exclude' && $zipcode_target ) {
@@ -262,7 +262,7 @@ class GeotWP_Helper {
 		extract( $opts );
 
 		if ( empty( $in_zipcodes ) && empty( $ex_zipcodes ) &&
-			count( $in_zips_regions ) == 0 && count( $ex_zips_regions ) == 0
+		     count( $in_zips_regions ) == 0 && count( $ex_zips_regions ) == 0
 		) {
 			return true;
 		}
@@ -825,6 +825,7 @@ class GeotWP_Helper {
 				return true;
 			}
 		}
+
 		return false;
 	}
 	/*
@@ -852,26 +853,28 @@ class GeotWP_Helper {
  * http://www.pgregg.com/donate/
  *
  * Please do not remove this header, or source attibution from this file.
- */
-
-	/**
-	 * ip_in_range
-	 * This function takes 2 arguments, an IP address and a "range" in several
-	 * different formats.
-	 * Network ranges can be specified as:
-	 * 1. Wildcard format:     1.2.3.*
-	 * 2. CIDR format:         1.2.3/24  OR  1.2.3.4/255.255.255.0
-	 * 3. Start-End IP format: 1.2.3.0-1.2.3.255
-	 * The function will return true if the supplied IP is within the range.
-	 * Note little validation is done on the range inputs - it expects you to
-	 * use one of the above 3 formats.
-	 *
-	 * @param $ip
-	 * @param $range
-	 *
-	 * @return bool
-	 */
-	public static function ip_in_range( $ip, $range ) {
+ * Modified by James Greene <james@cloudflare.com> to include IPV6 support
+* (original version only supported IPV4).
+* 21 May 2012
+*/
+// decbin32
+// In order to simplify working with IP addresses (in binary) and their
+// netmasks, it is easier to ensure that the binary strings are padded
+// with zeros out to 32 characters - IP addresses are 32 bit numbers
+	private static function decbin32( $dec ) {
+		return str_pad( decbin( $dec ), 32, '0', STR_PAD_LEFT );
+	}
+// ipv4_in_range
+// This function takes 2 arguments, an IP address and a "range" in several
+// different formats.
+// Network ranges can be specified as:
+// 1. Wildcard format:     1.2.3.*
+// 2. CIDR format:         1.2.3/24  OR  1.2.3.4/255.255.255.0
+// 3. Start-End IP format: 1.2.3.0-1.2.3.255
+// The function will return true if the supplied IP is within the range.
+// Note little validation is done on the range inputs - it expects you to
+// use one of the above 3 formats.
+	private static function ipv4_in_range( $ip, $range ) {
 		if ( strpos( $range, '/' ) !== false ) {
 			// $range is in IP/NETMASK format
 			list( $range, $netmask ) = explode( '/', $range, 2 );
@@ -920,9 +923,128 @@ class GeotWP_Helper {
 				return ( ( $ip_dec >= $lower_dec ) && ( $ip_dec <= $upper_dec ) );
 			}
 
-			// not a valid range
 			return false;
 		}
+	}
 
+	private static function ip2long6( $ip ) {
+		if ( substr_count( $ip, '::' ) ) {
+			$ip = str_replace( '::', str_repeat( ':0000', 8 - substr_count( $ip, ':' ) ) . ':', $ip );
+		}
+
+		$ip   = explode( ':', $ip );
+		$r_ip = '';
+		foreach ( $ip as $v ) {
+			$r_ip .= str_pad( base_convert( $v, 16, 2 ), 16, 0, STR_PAD_LEFT );
+		}
+
+		return base_convert( $r_ip, 2, 10 );
+	}
+
+// Get the ipv6 full format and return it as a decimal value.
+	private static function get_ipv6_full( $ip ) {
+		$pieces      = explode( "/", $ip, 2 );
+		$left_piece  = $pieces[0];
+		$right_piece = $pieces[1];
+		// Extract out the main IP pieces
+		$ip_pieces     = explode( "::", $left_piece, 2 );
+		$main_ip_piece = $ip_pieces[0];
+		$last_ip_piece = $ip_pieces[1];
+		// Pad out the shorthand entries.
+		$main_ip_pieces = explode( ":", $main_ip_piece );
+		foreach ( $main_ip_pieces as $key => $val ) {
+			$main_ip_pieces[ $key ] = str_pad( $main_ip_pieces[ $key ], 4, "0", STR_PAD_LEFT );
+		}
+		// Check to see if the last IP block (part after ::) is set
+		$last_piece = "";
+		$size       = count( $main_ip_pieces );
+		if ( trim( $last_ip_piece ) != "" ) {
+			$last_piece = str_pad( $last_ip_piece, 4, "0", STR_PAD_LEFT );
+
+			// Build the full form of the IPV6 address considering the last IP block set
+			for ( $i = $size; $i < 7; $i ++ ) {
+				$main_ip_pieces[ $i ] = "0000";
+			}
+			$main_ip_pieces[7] = $last_piece;
+		} else {
+			// Build the full form of the IPV6 address
+			for ( $i = $size; $i < 8; $i ++ ) {
+				$main_ip_pieces[ $i ] = "0000";
+			}
+		}
+
+		// Rebuild the final long form IPV6 address
+		$final_ip = implode( ":", $main_ip_pieces );
+
+		return self::ip2long6( $final_ip );
+	}
+// Determine whether the IPV6 address is within range.
+// $ip is the IPV6 address in decimal format to check if its within the IP range created by the cloudflare IPV6 address, $range_ip.
+// $ip and $range_ip are converted to full IPV6 format.
+// Returns true if the IPV6 address, $ip,  is within the range from $range_ip.  False otherwise.
+	private static function ipv6_in_range( $ip, $range_ip ) {
+		$pieces      = explode( "/", $range_ip, 2 );
+		$left_piece  = $pieces[0];
+		$right_piece = $pieces[1];
+		// Extract out the main IP pieces
+		$ip_pieces     = explode( "::", $left_piece, 2 );
+		$main_ip_piece = $ip_pieces[0];
+		$last_ip_piece = $ip_pieces[1];
+		// Pad out the shorthand entries.
+		$main_ip_pieces = explode( ":", $main_ip_piece );
+		foreach ( $main_ip_pieces as $key => $val ) {
+			$main_ip_pieces[ $key ] = str_pad( $main_ip_pieces[ $key ], 4, "0", STR_PAD_LEFT );
+		}
+		// Create the first and last pieces that will denote the IPV6 range.
+		$first = $main_ip_pieces;
+		$last  = $main_ip_pieces;
+		// Check to see if the last IP block (part after ::) is set
+		$last_piece = "";
+		$size       = count( $main_ip_pieces );
+		if ( trim( $last_ip_piece ) != "" ) {
+			$last_piece = str_pad( $last_ip_piece, 4, "0", STR_PAD_LEFT );
+
+			// Build the full form of the IPV6 address considering the last IP block set
+			for ( $i = $size; $i < 7; $i ++ ) {
+				$first[ $i ] = "0000";
+				$last[ $i ]  = "ffff";
+			}
+			$main_ip_pieces[7] = $last_piece;
+		} else {
+			// Build the full form of the IPV6 address
+			for ( $i = $size; $i < 8; $i ++ ) {
+				$first[ $i ] = "0000";
+				$last[ $i ]  = "ffff";
+			}
+		}
+		// Rebuild the final long form IPV6 address
+		$first    = self::ip2long6( implode( ":", $first ) );
+		$last     = self::ip2long6( implode( ":", $last ) );
+		$in_range = ( $ip >= $first && $ip <= $last );
+
+		return $in_range;
+	}
+
+	private static function ip_range_type( $range_ip ) {
+		if ( strpos( $range_ip, ':' ) !== false ) {
+			//if ip range provided is ipv6 then
+			//echo "range provided is ipv6";
+			return 1;
+		} else {
+			//if ip range provided is ipv4 then
+			//echo "range provided is ipv4";
+			return 2;
+		}
+	}
+
+	public static function ip_in_range( $ip, $range_ip ) {
+		//only check IPv4 addresses against IPv4 ranges
+		if ( filter_var( $ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) && self::ip_range_type( $range_ip ) == 2 ) {
+			return self::ipv4_in_range( $ip, $range_ip );
+		}
+		//only check IPv6 addresses against IPv6 ranges
+		if ( filter_var( $ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6 ) && self::ip_range_type( $range_ip ) == 1 ) {
+			return self::ipv6_in_range( $ip, $range_ip );
+		}
 	}
 }
