1d37be00d4be3c99100e2ec08d09ad1e97c40c7f
Damian Logghe <info@timersys.com>
2020-12-28 12:15:21 -0300

    fix for price per country plugin changes

--- includes/class-geot-wc.php
+++ includes/class-geot-wc.php
@@ -18,7 +18,10 @@ class GeotWP_WC {
 		add_filter( 'woocommerce_general_settings', [ $this, 'add_geolocation_field' ], 10, 1 );
 
 		// Default Customer Location
-		add_filter( 'woocommerce_get_geolocation', [ $this, 'apply_geolocation' ], 10, 2 );
+		add_filter( 'woocommerce_geolocate_ip', [ $this, 'apply_geolocation' ], 100, 2 );
+
+		// Prices per country plugin
+		add_filter( 'woocommerce_customer_get_billing_country', [ $this, 'apply_geolocation' ], 100, 2 );
 	}
 
 	public function add_geolocation_field( $fields = [] ) {
@@ -58,17 +61,6 @@ class GeotWP_WC {
 		if( empty( $country_code ) )
 			return $location;
 
-		$state_code = geot_state_code();
-		$city_name = geot_city_name();
-		$zip_code = geot_zip();
-
-		$location = [
-			'country'	=> $country_code,
-			'state'		=> $state_code,
-			'city'		=> $city_name,
-			'postcode'	=> $zip_code,
-		];
-
-		return apply_filters('geot/woocommerce/geolocation', $location, $ip );
+		return apply_filters('geot/woocommerce/geolocation', $country_code, $ip );
 	}
 }
