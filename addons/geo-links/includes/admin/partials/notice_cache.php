e127c2ccf8bb9233688b9e736c0a498059919dde
Damian Logghe <info@timersys.com>
2021-07-08 15:52:43 -0300

    fixed cache message

--- addons/geo-links/includes/admin/partials/notice_cache.php
+++ addons/geo-links/includes/admin/partials/notice_cache.php
@@ -4,7 +4,7 @@ $settings = geotWPL_settings();
 <div class="geot-notice notice notice-error is-dismissible" data-notice-id="geolinks-cache">
 	<h3><i class=" dashicons-before dashicons-admin-site"></i> GeotWP_Links</h3>
 	<p><?php _e( 'We detected that your have a cache plugin active.', 'geot' ); ?></p>
-	<p><?php echo sprintf( __( 'Please be sure to whitelist the Geolinks root url %s in your cache plugin.', 'geot' ), $settings['goto_page'] .'/' ); ?></p>
+	<p><?php echo sprintf( __( 'Please be sure to whitelist in your cache plugin the Geolinks root url: %s ', 'geot' ), $settings['goto_page'] .'/' ); ?></p>
 </div>
 <script>
     jQuery(document).on('click', '.geot-notice .notice-dismiss', function () {
